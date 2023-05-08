28296b6a69430f9c37c522bf2d76451221481b5e
Janpietermanteleers <jp.manteleers@gmail.com>
2023-05-08 17:19:47 +0200

    whoa

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -5,10 +5,12 @@
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
   </script>
+
   
   <svg {width} {height}>
     <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
+      <rect x="{0}" y="48" width="40" height="40" />
+      <circle cx = "0" cy = "0" r= "20" />
     </g>
   </svg>
   
@@ -16,6 +18,14 @@
     svg {
       border: 1px solid black;
       border-radius: 5px;
+    }
+
+    rect{
+      fill: green
+    }
+
+    circle{
+      fill: steelblue
     }
   </style>
   
--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -1,7 +1,12 @@
+
+
 <script>
+
+    import{scaleLinear} from 'd3-scale'
     // Dimensions
     const width = 800;
     const height = 100;
+    const scale = scaleLinear().domain([0, height]).range([0, width]);
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
@@ -12,7 +17,11 @@
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      <circle cx="{scale(values[0])}"   cy="{innerHeight/2}"   r="10" />
+      <circle cx="{scale(values[1])}" cy="{innerHeight/2}"   r="10" />
+      <circle cx="{scale(values[2])}"   cy="{innerHeight/2}"  r="10" />
+      <circle cx="{scale(values[3])}"  cy="{innerHeight/2}"   r="10" />
+      <circle cx="{scale(values[4])}"   cy="{innerHeight/2}" r="10" />
     </g>
   </svg>
   
